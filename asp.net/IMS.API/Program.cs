f58452a677999eb2717620ca7342a7e2299e2b3f
ducNT <cavoixanh2112@gmail.com>
2023-12-16 21:08:09 +0700

    add authorize for swagger

--- asp.net/IMS.API/Program.cs
+++ asp.net/IMS.API/Program.cs
@@ -3,26 +3,27 @@
 using IMS.BusinessService;
 using IMS.BusinessService.Common;
 using IMS.Infrastructure;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
 
 namespace IMS.Api
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-			var configuration = builder.Configuration;
-			// Add services to the container.
-			builder.Services.AddHttpContextAccessor();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
+            // Add services to the container.
+            builder.Services.AddHttpContextAccessor();
 
-			builder.Services.ConfigureInfrastructureServices(configuration);
-			builder.Services.ConfigureIdentityServices(configuration);
-			builder.Services.ConfigureApplicationServices();
-			builder.Services.ConfigureInfrastructureServices(configuration);
+            builder.Services.ConfigureInfrastructureServices(configuration);
+            builder.Services.ConfigureIdentityServices(configuration);
+            builder.Services.ConfigureApplicationServices();
+            builder.Services.ConfigureInfrastructureServices(configuration);
 
-			builder.Services.Configure<JwtSetting>(configuration.GetSection("JwtSettings"));
+            builder.Services.Configure<JwtSetting>(configuration.GetSection("JwtSettings"));
 
             var IMSCorsPolicy = "IMSCorsPolicy";
             builder.Services.AddCors(o => o.AddPolicy(IMSCorsPolicy, builder =>
@@ -35,9 +36,46 @@ namespace IMS.Api
             builder.Services.ConfigureSettingServices(configuration);
             builder.Services.AddHttpClient();
             builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                // infor of swagger
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Hr LeaveManagement Api",
+                    Version = "v1",
+                });
+                // define swaggger auth
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
             //
 
@@ -53,25 +91,25 @@ namespace IMS.Api
 
             var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline.
 
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-				
-			}
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+            }
 
 
             app.UseCors(IMSCorsPolicy);
             app.UseHttpsRedirection();
-			app.UseAuthentication();
-			app.UseAuthorization();
-			
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.MapControllers();
 
-			app.Run();
-		}
-	}
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
